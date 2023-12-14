d7ca7a14d22674b4d6e8a676d34843ce33aa5694
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-14 19:51:37 -0300

    Created login page

---
+++ application/views/auth/login.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="row py-5">
+        <div class="col-4 offset-4 py-5">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title py-2 text-center">Login</h5>
+                    <form>
+                        <div class="form-group">
+                            <label for="nick">Nombre de usuario</label>
+                            <input type="text" class="form-control" id="nick" name="nick">
+                        </div>
+                        <div class="form-group">
+                            <label for="clave">Clave</label>
+                            <input type="password" class="form-control" id="clave" name="clave">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
