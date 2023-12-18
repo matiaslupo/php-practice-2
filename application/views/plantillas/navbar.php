995757114f24f369f70d696cba68120bf4139bbf
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-12-18 16:01:28 -0300

    Added logout button

--- application/views/plantillas/navbar.php
+++ application/views/plantillas/navbar.php
@@ -7,15 +7,15 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link">Home</a>
+        <a class="nav-link">Inicio</a>
       </li>
       <li class="nav-item">
         <a class="nav-link">Link</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link ">Disabled</a>
+        <a class="nav-link ">Link 2</a>
       </li>
     </ul>
-    <button class="btn btn-outline-success my-2 my-sm-0" type="button">Search</button>
+    <button class="btn btn-outline-danger btn-sm my-2 my-sm-0" type="button"><i class="bi bi-box-arrow-right"></i></button>
   </div>
 </nav>
